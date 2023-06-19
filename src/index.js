975555ab0755060d69398ea00614e38ecab291bc
stk346 <stk346@gamil.com>
2023-06-19 17:36:26 +0900

    컴포넌트의 생명주기

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter03/Library';
 import Clock from './chapter04/Clock';
 import CommentList from './chapter05/CommentList';
+import NotificationList from './chapter06/NotificationList';
 
 const rootNode = document.getElementById('root');
 
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>,
 );
 
